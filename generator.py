2905f43664750d761778f4f730e9da081c57f411
Gary Cohen <gary.cohen@alan.eu>
2019-04-04 14:59:39 +0200

    use bitarray to store shift

--- generator.py
+++ generator.py
@@ -1,53 +1,58 @@
+from bitarray import bitarray
 from numpy.random import choice, shuffle
 
 
+NUMBER_OF_SHIFTS_PER_DAY = 4
+NUMBER_OF_MEMBERS_PER_SHIFT = 4
+
+
 class CareMember:
-    def __init__(self, name, level):
+    def __init__(self, name: str, level: str):
         self.name = name
         self.level = level
-        self.schedule = {"monday": 0, "tuesday": 0, "wednesday": 0, "thursday": 0,
-                         "friday": 0, "saturday": 0, "sunday": 0}
+        self.schedule = {"monday": bitarray(), "tuesday": bitarray(), "wednesday": bitarray(), "thursday": bitarray(),
+                         "friday": bitarray(), "saturday": bitarray(), "sunday": bitarray()}
 
-    def increment_shift(self, day):
-        self.schedule[day] += 1
+    def add_shift(self, day: str, add_to_schedule: bool):
+        self.schedule[day].append(add_to_schedule)
 
-    def number_of_shifts_yesterday(self, day):
-        if "monday" == day:
-            return self.schedule["sunday"]
-        elif "tuesday" == day:
-            return self.schedule["monday"]
-        elif "wednesday" == day:
-            return self.schedule["tuesday"]
-        elif "thursday" == day:
-            return self.schedule["wednesday"]
-        elif "friday" == day:
-            return self.schedule["thursday"]
-        elif "saturday" == day:
-            return self.schedule["friday"]
-        elif "sunday" == day:
-            return self.schedule["saturday"]
+    def number_of_shifts_yesterday(self, day: str) -> int:
+        if "monday" is day:
+            return self.schedule["sunday"].count()
+        elif "tuesday" is day:
+            return self.schedule["monday"].count()
+        elif "wednesday" is day:
+            return self.schedule["tuesday"].count()
+        elif "thursday" is day:
+            return self.schedule["wednesday"].count()
+        elif "friday" is day:
+            return self.schedule["thursday"].count()
+        elif "saturday" is day:
+            return self.schedule["friday"].count()
+        elif "sunday" is day:
+            return self.schedule["saturday"].count()
 
-    def schedule_weight(self, day, shift_number):
-        num_of_shifts = self.schedule[day]
-        w = 0 if num_of_shifts >= 2 else (abs(num_of_shifts - shift_number) + 1) ** (10 + (3-self.number_of_shifts_yesterday(day)))
-        # print("member: {}, shift number: {}, num of shifts: {}, num of shifts yesterday: {}, weight: {}".format(self.name, shift_number, num_of_shifts, self.number_of_shifts_yesterday(day), w))
+    def schedule_weight(self, day: str, shift_number: str) -> int:
+        num_of_shifts = self.schedule[day].count()
+        w = 0 if num_of_shifts >= 2 \
+            else (abs(num_of_shifts - shift_number) + 1) ** (10 + (3-self.number_of_shifts_yesterday(day)))
         return w
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.name
 
 
-def weights(members, day, shift_number):
+def weights(members: [CareMember], day: str, shift_number: int) -> (int, int):
     w = [member.schedule_weight(day, shift_number)for member in members]
     return w, sum(w)
 
 
-def probability(members, day, shift_number):
+def probability(members: [CareMember], day: str, shift_number: int) -> [int]:
     weights_for_day, total_weights = weights(members, day, shift_number)
     return [weight / total_weights for weight in weights_for_day]
 
 
-def generate_members_for_shift(members, day, shift_number, amount):
+def generate_members_for_shift(members: [CareMember], day: str, shift_number: int, amount: int) -> [CareMember]:
     if day is "monday" and shift_number is 0:
         return members
     elif shift_number is 0 or shift_number is 1:
@@ -56,14 +61,16 @@ def generate_members_for_shift(members, day, shift_number, amount):
         return sorted(members, key=lambda x: x.schedule_weight(day, shift_number))[-amount:]
 
 
-def schedule(members):
+def schedule(members: [CareMember]):
     for day in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
         print(day)
-        for shift_number in range(4):
+        for shift_number in range(NUMBER_OF_SHIFTS_PER_DAY):
             shuffle(members)
-            members_for_shift = generate_members_for_shift(members, day, shift_number, 4)
+            members_for_shift = generate_members_for_shift(members, day, shift_number, NUMBER_OF_MEMBERS_PER_SHIFT)
             for member in members_for_shift:
-                member.increment_shift(day)
+                member.add_shift(day, True)
+            for member in list(set(members) - set(members_for_shift)):
+                member.add_shift(day, False)
             print(sorted(members_for_shift, key=lambda x: x.name))
         print("")
 
@@ -84,7 +91,9 @@ def main():
 
     schedule(members)
     for member in members:
-        print("{} ({} shifts) [{}]\n{}\n".format(member.name, sum([member.schedule[day] for day in member.schedule]), member.level, member.schedule))
+        print("{} ({} shifts) [{}]\n{}\n".format(member.name,
+                                                 sum([member.schedule[day].count()
+                                                      for day in member.schedule]), member.level, member.schedule))
 
 
 main()
